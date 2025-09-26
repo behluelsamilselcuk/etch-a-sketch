f74b4525972b880f037a6966b6e6d341b8112566
Behlül Samil Selcuk <215264789+behluelsamilselcuk@users.noreply.github.com>
2025-09-26 22:51:39 +0200

    Ermögliche das Ändern von Quadratanzahl pro Seite
    Durch das Klicken auf den Button, soll man
    entscheiden können, wie viele Quadrate pro Seite
    vorhanden sein sollen.
    Nach Eingabe einer Zahl ändert sich das Raster.

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 'use strict';
 
 const container = document.querySelector('#container');
+const changeGridBtn = document.querySelector('#changeGridBtn');
+const containerLength = container.offsetWidth - 2;
+const etchASketch = document.querySelector('#etch-a-sketch');
 
-for (let i = 0; i < (16*16); i++) {
-    const grid = document.createElement('div');
-    grid.classList.add('grid');
-    grid.style.height = `${256/16}px`;
-    grid.style.width = `${256/16}px`;
+for (let i = 0; i < (16**2); i++) {
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        grid.style.height = `${containerLength/16}px`;
+        grid.style.width = `${containerLength/16}px`;
 
-    container.appendChild(grid);
+        container.appendChild(grid);
+    }
+
+function createGrid() {
+    const gridsToDel = document.querySelectorAll('.grid');
+    gridsToDel.forEach(grid => {
+        grid.remove();
+    });
+
+    const numOfGrids = prompt('Wie viele Quadrate pro Seite willst du haben?', '');
+
+    for (let i = 0; i < (numOfGrids**2); i++) {
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        grid.style.height = `${containerLength/numOfGrids}px`;
+        grid.style.width = `${containerLength/numOfGrids}px`;
+
+        container.appendChild(grid);
+    }
 }
 
+changeGridBtn.addEventListener('click', createGrid);
 
-const gridDivs = document.querySelectorAll('.grid');
-
-gridDivs.forEach(div => {
-    div.addEventListener('mouseenter', () => {
-        div.style.backgroundColor = 'grey';
-    })
-});
+etchASketch.addEventListener('mouseover', (e) => {
+    if((e.target).getAttribute('class') === 'grid') {
+        e.target.style.backgroundColor = 'grey';
+    }
+})
