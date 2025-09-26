9c424c25664a113c4bf335757ebb155e51110442
Behlül Samil Selcuk <215264789+behluelsamilselcuk@users.noreply.github.com>
2025-09-26 16:29:52 +0200

    Erstelle das 16x16-Raster
    Man braucht einen div#container, indem 16x16-Grids
    in den div#container platziert werden, sodass alle
    rein passen.

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 'use strict';
 
+const container = document.querySelector('#container');
+
+for (let i = 0; i < (16*16); i++) {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    grid.style.height = `${256/16}px`;
+    grid.style.width = `${256/16}px`;
+
+    container.appendChild(grid);
+}
